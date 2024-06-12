093621c22ae456dcfdf55aec966525128f7700b6
Lancherix <lancherix.service@gmail.com>
2024-06-12 11:04:31 -0500

    storage

--- lancherix-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ lancherix-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "contactAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "lancherixreactc0a89564": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "ContactStorage": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
--- lancherix-app/src/components/contacts/Contacts.js
+++ lancherix-app/src/components/contacts/Contacts.js
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+import { Storage /*, API, graphqlOperation*/ } from 'aws-amplify';
+
+import { createContact } from '../../graphql/mutations';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import {v4 as uuid} from 'uuid';
 
 function Contacts() {
+    const [contactData, setContactData] = useState({name: "", email: "", cell: ""});
+    const [profilePic, setProfilePic] = useState("");
+
+    const addNewContact = async () => {
+        const { name, email, cell } = contactData;
+
+        // Upload pic to S3
+        Storage.configure({ region: 'us-east-1' });
+        const { key } = await Storage.put(`${uuid()}.png`, profilePic, {contentType: 'image/png'});
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            cell,
+            profilePicPath: key
+        };
+
+        // Persist new Contact
+        // await API.graphql(graphqlOperation(createContact, {input: newContact}));
+    }
+
     return (
         <Container>
+            <Row className='px-4 my-5'>-
+            </Row>
+            <Row>-
+            </Row>
             <Row className='px-4 my-5'>
                 <Col><h1>Contacts</h1></Col>
             </Row>
@@ -20,26 +55,37 @@ function Contacts() {
                         </Card.Text>
                     </Card.Body>
                 </Card>
-                <Card style={{ width: '12rem' }}>
-                    <Card.Img src='/img/contact_2.png' variant='top' />
-                    <Card.Body>
-                        <Card.Title>Isabella Rodriguez</Card.Title>
-                        <Card.Text>
-                            Hello Isa
-                            <br />315.230.6864
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '12rem' }}>
-                    <Card.Img src='/img/contact_3.png' variant='top' />
-                    <Card.Body>
-                        <Card.Title>Isabella Rodriguez</Card.Title>
-                        <Card.Text>
-                            Hello Isa
-                            <br />315.230.6864
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+            </Row>
+            <Row className="px-4 my-5">
+                <Col sm={3}>
+                    <h2>Add New Contact</h2>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Contact name"
+                                value={contactData.name} 
+                                onChange={evt => setContactData({...contactData, name:evt.target.value})} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Contact email"
+                                value={contactData.email}
+                                onChange={evt => setContactData({...contactData, name:evt.target.value})} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label>Cell</Form.Label>
+                            <Form.Control type="text" placeholder="nnn-nnn-nnnn"
+                                value={contactData.cell}
+                                onChange={evt => setContactData({...contactData, name:evt.target.value})} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label>Profile Pic</Form.Label>
+                            <Form.Control type="file" accept="image/png"
+                                onChange={evt => setProfilePic(evt.target.files[0])} />
+                        </Form.Group>
+                        <Button variant="primary" type="button" onClick={addNewContact}>Add Contact &gt;&gt;</Button>&nbsp;
+                    </Form>
+                </Col>
             </Row>
         </Container>
     )
